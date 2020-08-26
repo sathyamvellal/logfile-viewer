932ecb388466e1b90006197294a385b23023511f
Sathyam Vellal <sathyam.vellal@gmail.com>
2020-08-25 20:49:34 -0700

    Build spdlog and test_spdlog executable

---
+++ test/test_spdlog.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Sathyam Vellal on 06/11/2017.
+//
+
+#include <iostream>
+#include "spdlog/spdlog.h"
+
+int main(int argc, char *argv[])
+{
+    spdlog::info("Hello, World!");
+    return 0;
+}
